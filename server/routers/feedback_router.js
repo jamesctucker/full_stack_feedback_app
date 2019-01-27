a13cd425c595e98380d489addbc3f8cb23773a14
James Tucker <jamesctucker@protonmail.com>
2019-01-27 13:08:02 -0600

    finished POST function in the router file

--- server/routers/feedback_router.js
+++ server/routers/feedback_router.js
@@ -10,9 +10,17 @@ const pool = new Pool({
     idleTimeoutMillis: 10000
 });
 
+// This router uses POST to transfer feedback data to the database
 router.post('/', (req, res) => {
-
-
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                    VALUES ($1,$2,$3,$4)`;
+    pool.query(queryText, [req.body.feelings, req.body.understanding, req.body.support,
+    req.body.comments]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(error, 'POST');
+        res.sendStatus(500);
+    });
 })
 
 
