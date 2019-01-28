481956957d0add116838573e502673d3d1656a9e
James Tucker <jamesctucker@protonmail.com>
2019-01-28 15:59:31 -0600

    added build folder for Heroku

---
+++ build/precache-manifest.140fa5efd79092ef62d07cfffdab534d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae94dc502cda50d08a46",
+    "url": "/static/css/main.62bd8226.chunk.css"
+  },
+  {
+    "revision": "ae94dc502cda50d08a46",
+    "url": "/static/js/main.ae94dc50.chunk.js"
+  },
+  {
+    "revision": "0d8f6f2d9f062d9de4b0",
+    "url": "/static/js/1.0d8f6f2d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22bcc78b6a95eb6eca779fd6a566fd0a",
+    "url": "/index.html"
+  }
+];
